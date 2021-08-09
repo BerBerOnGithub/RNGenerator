976f3e93f024f42235fa30982cd0fbf7692edb59
BerBerOnGithub <86917016+BerBerOnGithub@users.noreply.github.com>
2021-08-09 14:54:26 +0200

    Add files via upload

---
+++ RNGenerator.bat
@@ -0,0 +1,39 @@
+@echo off
+:generator
+@echo RNG Generator
+@echo.
+@echo This app generates a random number using RNG.
+@echo.
+@echo Your generated number is %random%.
+@echo.
+@echo If you want to generate another number, do the instructions below:
+pause
+cls
+@echo Generating new number.
+timeout 1 > nul
+cls
+@echo Generating new number..
+timeout 1 > nul
+cls
+@echo Generating new number...
+timeout 1 > nul
+cls
+@echo Generating new number.
+timeout 1 > nul
+cls
+@echo Generating new number..
+timeout 1 > nul
+cls
+@echo Generating new number...
+timeout 1 > nul
+cls
+@echo Generating new number.
+timeout 1 > nul
+cls
+@echo Generating new number..
+timeout 1 > nul
+cls
+@echo Generating new number...
+timeout 1 > nul
+cls
+goto generator
